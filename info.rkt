84cb54dfb27f73cadc2e571ccd35ae31ad648b52
Andrei Formiga <archimedes_siracusa@hotmail.com>
2018-06-28 12:42:14 -0300

    Add package info

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "rasdel")
+(define deps '("base"))
+#;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+#;(define scribblings '(("scribblings/rasdel.scrbl" ())))
+(define pkg-desc "Racket + SDL")
+(define version "0.0")
+(define pkg-authors '(andrei))
